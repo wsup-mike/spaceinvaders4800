b7e98f98aa87e1a177e779407871f8ecdedb3b9e
Mike R <or4ngedeuce@gmail.com>
2022-05-15 19:18:36 -0500

    another commit

--- contracts/SpaceInvaders4800.sol
+++ contracts/SpaceInvaders4800.sol
@@ -1,22 +1,56 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Greeter {
-    string private greeting;
+contract SpaceInvaders4800 is ERC721, Ownable {
+	uint256 public mintPrice;
+	uint256 public totalSupply;
+	uint256 public maxSupply;
+	uint256 public maxPerWallet;
+	bool public isPublicMintEnabled;
+	string internal baseTokenUri;
+	address payable public withdrawWallet;
+	mapping(address => uint256) public walletMints;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
+	constructor() payable ERC721('SpaceInvaders4800', 'SPACE4800') {
+		mintPrice = 0.5 ether;
+		totalSupply = 0;
+		maxSupply = 1000;
+		maxPerWallet = 3;
+	}
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+	function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner {
+		isPublicMintEnabled = isPublicMintEnabled_;
+	}
+
+	function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner {
+		baseTokenUri = baseTokenUri_;
+	}
+
+	function tokenURI(uint256 tokenId_) public view override returns (string memory) {
+		require(_exists(tokenId_), 'Token does not exist!');
+		return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));		
+	}
+
+	function withdraw() external onlyOwner {
+		(bool success, ) = withdrawWallet.call{ value: address(this).balance }('');
+		require(success, 'withdraw failed');
+	}
+
+	function mint(uint256 quantity_) public payable {
+		require(isPublicMintEnabled, 'minting not enable');
+		require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+		require(totalSupply + quantity_ <= maxSupply, 'sold out');
+		require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+		for (uint256 i = 0; i < quantity_; i++) {
+			uint256 newTokenId = totalSupply + 1;
+			totalSupply++;
+			_safeMint(msg.sender, newTokenId);
+		}
+	}
 }
+
